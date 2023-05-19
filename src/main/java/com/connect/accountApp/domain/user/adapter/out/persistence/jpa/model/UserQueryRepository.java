61b9ddbbae3f1ddf3672b6ab75293bb748a78994
eunhee <dmsgml9903@gmail.com>
2023-05-19 14:18:44 +0900

    [FEAT] send-money 기능에서 룸메이트의 정보를 조회하는 기능 구현

---
+++ src/main/java/com/connect/accountApp/domain/user/adapter/out/persistence/jpa/model/UserQueryRepository.java
@@ -0,0 +1,50 @@
+package com.connect.accountApp.domain.user.adapter.out.persistence.jpa.model;
+
+import static com.connect.accountApp.domain.user.adapter.out.persistence.jpa.model.QUserJpaEntity.userJpaEntity;
+
+import com.connect.accountApp.domain.user.application.port.out.command.RoommateSendMoneyCommand;
+import com.querydsl.core.types.Projections;
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+@RequiredArgsConstructor
+@Component
+@Slf4j
+public class UserQueryRepository {
+
+  private final JPAQueryFactory jpaQueryFactory;
+
+  public List<RoommateSendMoneyCommand> getRoommateSendMoney(Long householdId, Long userId) {
+
+    return jpaQueryFactory
+        .select(Projections.constructor(RoommateSendMoneyCommand.class,
+            userJpaEntity.userId,
+            userJpaEntity.userName,
+            userJpaEntity.userRatio,
+            userJpaEntity.userAccountBank,
+            userJpaEntity.userAccount
+        ))
+        .from(userJpaEntity)
+        .where(
+            eqHouseholdId(householdId),
+            notInUserId(userId)
+        )
+        .fetch();
+
+  }
+
+  private BooleanExpression eqHouseholdId(Long householdId) {
+    log.info("getRoommateSendMoney => householdId : {}", householdId);
+    return householdId != null ? userJpaEntity.houseHoldJpaEntity.householdId.eq(householdId) : null;
+  }
+
+  private BooleanExpression notInUserId(Long userId) {
+    log.info("getRoommateSendMoney => userId : {}", userId);
+    return userId != null ? userJpaEntity.userId.notIn(userId) : null;
+  }
+
+}
