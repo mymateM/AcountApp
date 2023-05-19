5b744378f2ca8e16b3c7974b64b3eb916800bb74
eunhee <dmsgml9903@gmail.com>
2023-05-20 00:32:26 +0900

    [ADD] 배포 자동화 구성

--- src/main/java/com/connect/accountApp/AccountAppApplication.java
+++ src/main/java/com/connect/accountApp/AccountAppApplication.java
@@ -2,12 +2,15 @@ package com.connect.accountApp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.ApplicationPidFileWriter;
 
 @SpringBootApplication
 public class AccountAppApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AccountAppApplication.class, args);
+		SpringApplication application = new SpringApplication(AccountAppApplication.class);
+		application.addListeners(new ApplicationPidFileWriter());
+		application.run(args);
 	}
 
 }
