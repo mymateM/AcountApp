051539511bf0f4a331c5e93170e26b8afc8e2324
eunhee <dmsgml9903@gmail.com>
2023-05-24 22:49:01 +0900

    [CHORE] enum 클래스인 ExpenseCategory에서 상수에 값 설정

--- src/main/java/com/connect/accountApp/domain/expense/domain/model/ExpenseCategory.java
+++ src/main/java/com/connect/accountApp/domain/expense/domain/model/ExpenseCategory.java
@@ -1,6 +1,26 @@
 package com.connect.accountApp.domain.expense.domain.model;
 
-public enum ExpenseCategory {
+import com.connect.accountApp.global.common.service.EnumMapperType;
+import lombok.Getter;
 
-  FOOD, SHOPPING, TRANSPORT, MEDICAL, HOUSEITEM, ENTERTAINMENT, SAVE, EDUCATION
+@Getter
+public enum ExpenseCategory implements EnumMapperType {
+
+  FOOD("식비", "FOOD.jpg"),
+  SHOPPING("쇼핑", "SHOPPING.jpg"),
+  TRANSPORT("교통", "TRANSPORT.jpg"),
+  MEDICAL("의료", "MEDICAL.jpg"),
+  HOUSE_ITEM("생활", "HOUSE_ITEM.jpg"),
+  ENTERTAINMENT("오락", "ENTERTAINMENT.jpg"),
+  SAVE("저축", "SAVE.jpg"),
+  EDUCATION("교육", "EDUCATION.jpg"),
+  ETC("기타", "ETC.jpg");
+
+  private String title;
+  private String imgUrl;
+
+  ExpenseCategory(String title, String imgUrl) {
+    this.title = title;
+    this.imgUrl = imgUrl;
+  }
 }
