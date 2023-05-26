456571d6c58c7c67afeaeb5b0afe0572cf72b744
eunhee <dmsgml9903@gmail.com>
2023-05-26 22:45:33 +0900

    [FEAT] 가구의 user를 구하는 기능 구현

--- src/main/java/com/connect/accountApp/domain/user/adapter/out/persistence/UserJpaRepository.java
+++ src/main/java/com/connect/accountApp/domain/user/adapter/out/persistence/UserJpaRepository.java
@@ -1,10 +1,12 @@
 package com.connect.accountApp.domain.user.adapter.out.persistence;
 
+import com.connect.accountApp.domain.household.adapter.out.persistence.jpa.model.HouseHoldJpaEntity;
 import com.connect.accountApp.domain.user.adapter.out.persistence.jpa.model.UserJpaEntity;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserJpaRepository extends JpaRepository<UserJpaEntity, Long> {
 
-
+  List<UserJpaEntity> findAllByHouseHoldJpaEntity(HouseHoldJpaEntity jpaEntity);
 
 }
---
+++ src/main/java/com/connect/accountApp/domain/user/application/port/out/FindHouseholdUserListPort.java
@@ -0,0 +1,11 @@
+package com.connect.accountApp.domain.user.application.port.out;
+
+import com.connect.accountApp.domain.household.domain.model.Household;
+import com.connect.accountApp.domain.user.domain.model.User;
+import java.util.List;
+
+public interface FindHouseholdUserListPort {
+
+  List<User> findHouseholdUserList(Household household);
+
+}
