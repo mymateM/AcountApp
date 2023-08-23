0591f4dce04972420c127cde5c50b605b96f3544
eunhee <dmsgml9903@gmail.com>
2023-08-23 09:39:28 +0900

    feat(jwtService): jwt 토큰 생성, 추출 및 검증을 위한 서비스 구현

--- src/main/java/com/connect/accountApp/domain/user/adapter/out/persistence/UserJpaRepository.java
+++ src/main/java/com/connect/accountApp/domain/user/adapter/out/persistence/UserJpaRepository.java
@@ -3,10 +3,13 @@ package com.connect.accountApp.domain.user.adapter.out.persistence;
 import com.connect.accountApp.domain.household.adapter.out.persistence.jpa.model.HouseHoldJpaEntity;
 import com.connect.accountApp.domain.user.adapter.out.persistence.jpa.model.UserJpaEntity;
 import java.util.List;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserJpaRepository extends JpaRepository<UserJpaEntity, Long> {
 
   List<UserJpaEntity> findAllByHouseHoldJpaEntity(HouseHoldJpaEntity jpaEntity);
+
+  Optional<UserJpaEntity> findByUserEmail(String userEmail);
 
 }
---
+++ src/main/java/com/connect/accountApp/global/common/service/JwtService.java
@@ -0,0 +1,79 @@
+package com.connect.accountApp.global.common.service;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import java.security.Key;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+@Service
+public class JwtService {
+
+  private static final String SECRET_KEY = "8f0bd83363c7a8a609eaf046b324283236616a9b8373085999b4b1ba2ce3c993315e6281dc53cf9d12a112909a944a065d6244a2efbdeeac11d3f97ed43b1f55";
+
+
+  public String extractUsername(String token) {
+    return extractClaim(token, Claims::getSubject);
+  }
+
+
+  public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+    final Claims claims = extractAllClaims(token);
+    return claimsResolver.apply(claims);
+  }
+
+  public String generateToken(UserDetails userDetails) {
+    return generateToken(new HashMap<>(), userDetails);
+  }
+
+  public String generateToken(String username) {
+    return generateToken(new HashMap<>(), username);
+  }
+
+  public String generateToken(Map<String, Object> extraClaims, UserDetails userDetails) {
+    return Jwts
+        .builder()
+        .setClaims(extraClaims)
+        .setSubject(userDetails.getUsername())
+        .setIssuedAt(new Date(System.currentTimeMillis()))
+        .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 24))
+        .signWith(getSignInKey(), SignatureAlgorithm.ES256)
+        .compact();
+  }
+
+  public boolean isTokenValid(String token, UserDetails userDetails) {
+    final String username = extractUsername(token);
+    return (username.equals(userDetails.getUsername())) && !isExpiredToken(token);
+  }
+
+  private boolean isExpiredToken(String token) {
+    return extractExpiration(token).before(new Date());
+  }
+
+  private Date extractExpiration(String token) {
+    return extractClaim(token, Claims::getExpiration);
+  }
+  private Claims extractAllClaims(String token) {
+    return Jwts
+        .parserBuilder()
+        .setSigningKey(getSignInKey())
+        .build()
+        .parseClaimsJws(token)
+        .getBody();
+  }
+
+
+  private Key getSignInKey() {
+    byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+    return Keys.hmacShaKeyFor(keyBytes);
+  }
+
+
+}
