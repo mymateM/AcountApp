dc1dff82684658d3acbf9544202e5f2686add56e
eunhee <dmsgml9903@gmail.com>
2023-08-29 13:34:57 +0900

    [CHORE] 응답 형식 변경

--- src/main/java/com/connect/accountApp/domain/user/adapter/in/web/response/AuthenticationResponse.java
+++ src/main/java/com/connect/accountApp/domain/user/adapter/in/web/response/AuthenticationResponse.java
@@ -1,5 +1,6 @@
 package com.connect.accountApp.domain.user.adapter.in.web.response;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -11,7 +12,10 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class AuthenticationResponse {
 
+  @JsonProperty("access_token")
   private String accessToken;
+
+  @JsonProperty("refresh_token")
   private String refreshToken;
 
 }
