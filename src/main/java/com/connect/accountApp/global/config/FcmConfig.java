dbc1e8e0be4eb6cee7adb8ec754b19b936666634
eunhee <dmsgml9903@gmail.com>
2023-09-10 15:26:41 +0900

    [FEAT] 배포시 firebase admin sdk 경로변경에 대응하여 @value 사용

--- src/main/java/com/connect/accountApp/global/config/FcmConfig.java
+++ src/main/java/com/connect/accountApp/global/config/FcmConfig.java
@@ -4,24 +4,22 @@ import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import com.google.firebase.messaging.FirebaseMessaging;
+import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.util.List;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.ClassPathResource;
 
 @Configuration
 public class FcmConfig {
 
+  @Value("${spring.application.fcm.admin-sdk}")
+  public String adminSdk;
+
   @Bean
   FirebaseMessaging firebaseMessaging() throws IOException {
-    // ec2 배포시 FileInputStream은 기존의 절대 경로를 사용하기 때문에 문제 발생 -> ClassPathResource 사용
-    ClassPathResource resource = new ClassPathResource(
-        "firebase/fcm-mymate-firebase-adminsdk-zjwbt-aa9e97d875.json");
-
-    InputStream refreshToken = resource.getInputStream();
-
+    FileInputStream refreshToken = new FileInputStream(adminSdk);
     FirebaseApp firebaseApp = null;
 
     List<FirebaseApp> firebaseAppList = FirebaseApp.getApps();
