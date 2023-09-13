e630c42f4abeb77223e70485d95b8a620cfb8246
eunhee <dmsgml9903@gmail.com>
2023-09-13 12:18:08 +0900

    [FIX] 이메일 형식 검증 추가

--- src/main/java/com/connect/accountApp/domain/user/adapter/in/web/request/RegisterRequest.java
+++ src/main/java/com/connect/accountApp/domain/user/adapter/in/web/request/RegisterRequest.java
@@ -1,5 +1,6 @@
 package com.connect.accountApp.domain.user.adapter.in.web.request;
 
+import jakarta.validation.constraints.Email;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -14,6 +15,7 @@ public class RegisterRequest {
 
   private String nickname;
 
+  @Email(message = "이메일 형식이 올바르지 않습니다.")
   private String email;
 
   private String password;
