f73212f3c2c60fe44741c9e380b5f58372bce114
eunhee <dmsgml9903@gmail.com>
2023-09-13 14:19:50 +0900

    [ADD] CreatedResponse 응답 추가

--- src/main/java/com/connect/accountApp/global/common/adapter/in/web/response/SuccessResponse.java
+++ src/main/java/com/connect/accountApp/global/common/adapter/in/web/response/SuccessResponse.java
@@ -10,6 +10,7 @@ import lombok.Getter;
 public class SuccessResponse<T> {
 
   public static final String SUCCESS_MESSAGE = "Success";
+  public static final String CREATED_MESSAGE = "Created";
 
   private String message;
   private int status;
@@ -22,5 +23,9 @@ public class SuccessResponse<T> {
 
   public static <T> SuccessResponse create200SuccessResponse() {
     return new SuccessResponse(SUCCESS_MESSAGE, 200, null);
+  }
+
+  public static <T> SuccessResponse create201CreatedResponse() {
+    return new SuccessResponse(CREATED_MESSAGE, 200, null);
   }
 }
