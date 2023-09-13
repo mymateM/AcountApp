108af817e1486663eed5f5e3ff46c1e775e1c575
eunhee <dmsgml9903@gmail.com>
2023-09-13 16:19:34 +0900

    [FIX] NullPointerException 오류 수정  : 사용자가 가구에 속하지 않을 때 household를 매핑하는 경우 NullPointerException이 발생. 이를 해결

--- src/main/java/com/connect/accountApp/domain/user/adapter/out/persistence/jpa/model/UserJpaEntity.java
+++ src/main/java/com/connect/accountApp/domain/user/adapter/out/persistence/jpa/model/UserJpaEntity.java
@@ -7,6 +7,7 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -44,7 +45,7 @@ public class UserJpaEntity {
 
   private String deviceToken;
 
-  @ManyToOne
+  @ManyToOne(fetch = FetchType.LAZY)
   @JoinColumn(name = "household_id")
   private HouseHoldJpaEntity houseHoldJpaEntity;
 
