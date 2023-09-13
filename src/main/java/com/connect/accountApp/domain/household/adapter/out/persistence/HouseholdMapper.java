d690147ed885fc65f8ca97b00628b1cac19a6e4e
eunhee <dmsgml9903@gmail.com>
2023-09-13 15:09:18 +0900

    [CHORE] Household 객체에 필드 추가 및 수정 - 객체 생성일 : createdAt - 정산 일자 : householdSettlementDayOfMonth - 가구 이름 : householdName - 가구 예산 허용 범위 : householdBudgetAllowanceRatio - amount 타입을 BigDecimal로 수정

--- src/main/java/com/connect/accountApp/domain/household/adapter/out/persistence/HouseholdMapper.java
+++ src/main/java/com/connect/accountApp/domain/household/adapter/out/persistence/HouseholdMapper.java
@@ -10,18 +10,26 @@ public class HouseholdMapper {
   public Household mapToDomainEntity(HouseHoldJpaEntity houseHoldJpaEntity) {
     return Household.builder()
         .householdId(houseHoldJpaEntity.getHouseholdId())
+        .householdName(houseHoldJpaEntity.getHouseholdName())
         .householdSettlementDate(houseHoldJpaEntity.getHouseholdSettlementDate())
         .householdBudget(houseHoldJpaEntity.getHouseholdBudget())
+        .householdBudgetAllowanceRatio(houseHoldJpaEntity.getHouseholdBudgetAllowanceRatio())
         .householdAccept(houseHoldJpaEntity.isHouseholdAccept())
+        .createdAt(houseHoldJpaEntity.getCreatedAt())
+        .householdSettlementDayOfMonth(houseHoldJpaEntity.getHouseholdSettlementDayOfMonth())
         .build();
   }
 
   public HouseHoldJpaEntity mapToJpaEntity(Household household) {
     return HouseHoldJpaEntity.builder()
         .householdId(household.getHouseholdId())
+        .householdName(household.getHouseholdName())
         .householdSettlementDate(household.getHouseholdSettlementDate())
         .householdBudget(household.getHouseholdBudget())
+        .householdBudgetAllowanceRatio(household.getHouseholdBudgetAllowanceRatio())
         .householdAccept(household.isHouseholdAccept())
+        .createdAt(household.getCreatedAt())
+        .householdSettlementDayOfMonth(household.getHouseholdSettlementDayOfMonth())
         .build();
   }
 }
--- src/main/java/com/connect/accountApp/domain/household/adapter/out/persistence/jpa/model/HouseHoldJpaEntity.java
+++ src/main/java/com/connect/accountApp/domain/household/adapter/out/persistence/jpa/model/HouseHoldJpaEntity.java
@@ -5,11 +5,15 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.CurrentTimestamp;
 
 @Entity
 @Getter
@@ -19,17 +23,26 @@ public class HouseHoldJpaEntity {
 
   @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long householdId;
+  private String householdName;
   private LocalDate householdSettlementDate;
-  private int householdBudget;
+  private BigDecimal householdBudget;
+  private Integer householdBudgetAllowanceRatio;
   private boolean householdAccept;
 
+  @CreationTimestamp
+  private LocalDateTime createdAt;
+  private Integer householdSettlementDayOfMonth;
+
   @Builder
-  public HouseHoldJpaEntity(Long householdId, LocalDate householdSettlementDate,
-      int householdBudget,
-      boolean householdAccept) {
+  public HouseHoldJpaEntity(Long householdId, String householdName, LocalDate householdSettlementDate, BigDecimal householdBudget,
+      Integer householdBudgetAllowanceRatio, boolean householdAccept, LocalDateTime createdAt, Integer householdSettlementDayOfMonth) {
     this.householdId = householdId;
+    this.householdName = householdName;
     this.householdSettlementDate = householdSettlementDate;
     this.householdBudget = householdBudget;
+    this.householdBudgetAllowanceRatio = householdBudgetAllowanceRatio;
     this.householdAccept = householdAccept;
+    this.createdAt = createdAt;
+    this.householdSettlementDayOfMonth = householdSettlementDayOfMonth;
   }
 }
