83a79db677a89baa0944cad8489f55db6d2a34a0
eunhee <dmsgml9903@gmail.com>
2023-09-17 19:49:37 +0900

    [FEAT] 새로운 멤버가 가구원에 추가되면, 기존의 가구원에게 알림이 가는 로직 추가 및 구현

--- src/main/java/com/connect/accountApp/domain/household/adapter/in/web/controller/UserEnterHouseholdController.java
+++ src/main/java/com/connect/accountApp/domain/household/adapter/in/web/controller/UserEnterHouseholdController.java
@@ -1,6 +1,7 @@
 package com.connect.accountApp.domain.household.adapter.in.web.controller;
 
 import com.connect.accountApp.domain.household.application.port.in.AcceptingInvitiation;
+import com.connect.accountApp.domain.household.application.port.in.NotifyAcceptingInvitationUseCase;
 import com.connect.accountApp.global.common.adapter.in.web.response.SuccessResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -17,16 +18,16 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserEnterHouseholdController {
 
   private final AcceptingInvitiation acceptingInvitiation;
-//  private final NotiAcceptingInvitationUseCase notiAcceptingInvitationUseCase;
+  private final NotifyAcceptingInvitationUseCase notifyAcceptingInvitationUseCase;
 
   @GetMapping("/enter/{invite_code}")
   public ResponseEntity userEnterHousehold(@AuthenticationPrincipal UserDetails userDetails,
       @PathVariable("invite_code") String householdInviteCode) {
 
-
-
     String userEmail = userDetails.getUsername();
     acceptingInvitiation.userEnterHousehold(userEmail, householdInviteCode);
+
+    notifyAcceptingInvitationUseCase.notifyAcceptingInvitation(householdInviteCode, userEmail);
 
     return ResponseEntity.ok(SuccessResponse.create200SuccessResponse());
   }
