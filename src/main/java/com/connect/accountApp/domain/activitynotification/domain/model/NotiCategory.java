cdc4749e4d90d3f59c1b04ba4ffd7522cdbbe8c4
eunhee <dmsgml9903@gmail.com>
2023-09-17 19:46:41 +0900

    [CHORE] 초대 수락 알림 카테고리 추가 및 지출 알림 카테고리 삭제

--- src/main/java/com/connect/accountApp/domain/activitynotification/domain/model/NotiCategory.java
+++ src/main/java/com/connect/accountApp/domain/activitynotification/domain/model/NotiCategory.java
@@ -10,7 +10,7 @@ public enum NotiCategory implements EnumMapperType {
   REPORT_MONTH_SETTLEMENT("월별 리포트","report_month_settlement.jpg"),
   STORE_BILL("고지서 보관","store_bill.jpg"),
   WARN_OVER_BUDGET("예산 초과 경고","warn_over_budget.jpg"),
-  ALARM_EXPENSE("지출 알람","alarm_expense.jpg");
+  ACCEPT_INVITATION("초대 수락","accept_invitation.jpg");
 
 
   private String title;
