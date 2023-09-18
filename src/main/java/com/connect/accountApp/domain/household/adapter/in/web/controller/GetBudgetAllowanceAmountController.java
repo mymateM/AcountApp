1bb2dc520edea2b6bfd41edcff863dcbec7203a0
eunhee <dmsgml9903@gmail.com>
2023-09-18 15:40:45 +0900

    [FEAT] 예산 초과 허용 비율을 제공하면, 현재 예산대비 얼마까지 허용이 가능한 금액을 계산하는 API 구현

---
+++ src/main/java/com/connect/accountApp/domain/household/adapter/in/web/controller/GetBudgetAllowanceAmountController.java
@@ -0,0 +1,37 @@
+package com.connect.accountApp.domain.household.adapter.in.web.controller;
+
+import com.connect.accountApp.domain.household.adapter.in.web.response.GetBudgetAllowanceAmountResponse;
+import com.connect.accountApp.domain.household.application.port.in.GetBudgetAllowanceAmountUseCase;
+import com.connect.accountApp.global.common.adapter.in.web.response.SuccessResponse;
+import java.math.BigDecimal;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/household")
+@RequiredArgsConstructor
+public class GetBudgetAllowanceAmountController {
+
+  private final GetBudgetAllowanceAmountUseCase getBudgetAllowanceAmountUseCase;
+
+  @GetMapping("/budget/allowance-amount")
+  public ResponseEntity getBudgetAllowanceAmount(
+      @RequestParam("budget-amount") BigDecimal budgetAmount,
+      @RequestParam("allowance-ratio") Integer budgetAllowanceRatio) {
+
+    GetBudgetAllowanceAmountResponse response = getBudgetAllowanceAmountUseCase.getBudgetAllowanceAmount(budgetAmount, budgetAllowanceRatio);
+
+    return ResponseEntity.ok(SuccessResponse.create200SuccessResponse(response));
+
+
+  }
+
+
+}
