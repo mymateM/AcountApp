399df45d593cb80d9159c7202665a3883a71f05e
eunhee <dmsgml9903@gmail.com>
2023-09-21 11:22:10 +0900

    [FEAT] 지출을 데이터베이스에 저장하는 포트 구현

--- src/main/java/com/connect/accountApp/domain/expense/adapter/out/persistence/ExpensePersistenceAdapter.java
+++ src/main/java/com/connect/accountApp/domain/expense/adapter/out/persistence/ExpensePersistenceAdapter.java
@@ -1,10 +1,14 @@
 package com.connect.accountApp.domain.expense.adapter.out.persistence;
 
+import com.connect.accountApp.domain.expense.adapter.out.persistence.jpa.ExpenseJpaRepository;
+import com.connect.accountApp.domain.expense.adapter.out.persistence.jpa.model.ExpenseJpaEntity;
 import com.connect.accountApp.domain.expense.application.port.out.FindDailyTotalExpensesPort;
 import com.connect.accountApp.domain.expense.application.port.out.GetHouseholdTotalExpensePort;
 import com.connect.accountApp.domain.expense.application.port.out.GetTotalExpensePort;
+import com.connect.accountApp.domain.expense.application.port.out.SaveExpensePort;
 import com.connect.accountApp.domain.expense.application.port.out.command.DailyTotalExpensesCommand;
 import com.connect.accountApp.domain.expense.application.port.out.command.TotalExpenseCommand;
+import com.connect.accountApp.domain.expense.domain.model.Expense;
 import com.connect.accountApp.domain.user.application.port.out.GetUserSendMoneyPort;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
@@ -15,9 +19,11 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 @Component
 public class ExpensePersistenceAdapter implements GetTotalExpensePort, GetUserSendMoneyPort,
-    GetHouseholdTotalExpensePort, FindDailyTotalExpensesPort {
+    GetHouseholdTotalExpensePort, FindDailyTotalExpensesPort, SaveExpensePort {
 
   private final ExpenseQueryRepository expenseQueryRepository;
+  private final ExpenseJpaRepository expenseJpaRepository;
+  private final ExpenseMapper expenseMapper;
 
   @Override
   public List<TotalExpenseCommand> getTotalExpense(Long householdId, LocalDateTime startTime,
@@ -42,5 +48,12 @@ public class ExpensePersistenceAdapter implements GetTotalExpensePort, GetUserSe
   public List<DailyTotalExpensesCommand> FindDailyTotalExpenses(Long householdId, LocalDate date) {
 
     return expenseQueryRepository.getDailyTotalExpenseOfHousehold(householdId, date);
+  }
+
+  @Override
+  public Long saveExpensePort(Expense expense) {
+    ExpenseJpaEntity expenseJpaEntity = expenseMapper.mapToJpaEntity(expense);
+    Long expenseId = expenseJpaRepository.save(expenseJpaEntity).getExpenseId();
+    return expenseId;
   }
 }
