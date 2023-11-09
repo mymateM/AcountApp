826959127cdeb210aeae08b30856aad9be2e1c7d
eunhee <dmsgml9903@gmail.com>
2023-11-09 16:03:22 +0900

    feat(NotiCategory): 카테고리 추가

--- src/main/java/com/connect/accountApp/domain/activitynotification/domain/model/NotiCategory.java
+++ src/main/java/com/connect/accountApp/domain/activitynotification/domain/model/NotiCategory.java
@@ -11,6 +11,7 @@ public enum NotiCategory implements EnumMapperType {
   STORE_BILL("고지서 보관","store_bill.jpg"),
   WARN_OVER_BUDGET("예산 초과 경고","warn_over_budget.jpg"),
   ACCEPT_INVITATION("초대 수락","accept_invitation.jpg"),
+  REQUEST_SETTLEMENT("송금 요청","request_settlement.jpg"), //콕! 00님에게 이번 달 정산을 하러 가볼까요?
   WILL_UPDATE_BUDGET("예산 변경","update_budget.jpg"),
   UPDATED_BUDGET("예산 변경","updated_budget.jpg"),
   UPDATE_SETTLEMENT_DATE("정산일 변경","update_settlement_date.jpg");
