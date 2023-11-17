52fad73f33c49cab65769a788239cb0171fba11f
eunhee <dmsgml9903@gmail.com>
2023-11-17 20:59:43 +0900

    refactor : 지출 등록 수정
    : 정산 도메인 삭제로 인한 지출 등록 수정

--- src/main/java/com/connect/accountApp/domain/expense/adapter/in/web/controller/RegisterExpenseController.java
+++ src/main/java/com/connect/accountApp/domain/expense/adapter/in/web/controller/RegisterExpenseController.java
@@ -4,6 +4,7 @@ import com.connect.accountApp.domain.expense.adapter.in.web.controller.request.R
 import com.connect.accountApp.domain.expense.application.port.in.RegisterExpenseUseCase;
 import com.connect.accountApp.global.common.adapter.in.web.response.SuccessResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -15,14 +16,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/v1/expense")
 @RequiredArgsConstructor
+@Slf4j
 public class RegisterExpenseController {
 
   private final RegisterExpenseUseCase registerExpenseUseCase;
 
-
   @PostMapping("")
   public ResponseEntity registerExpense(@AuthenticationPrincipal UserDetails userDetails,
       @RequestBody RegisterExpenseRequest request) {
+
+    log.info("[controller] : {}", "RegisterExpenseController");
 
     String userEmail = userDetails.getUsername();
     Long expenseId = registerExpenseUseCase.registerExpense(userEmail, request);
--- src/main/java/com/connect/accountApp/domain/settlement/adapter/out/persistence/SettlementPersistenceAdapter.java
+++ src/main/java/com/connect/accountApp/domain/settlement/adapter/out/persistence/SettlementPersistenceAdapter.java
@@ -1,11 +1,8 @@
 package com.connect.accountApp.domain.settlement.adapter.out.persistence;
 
-import com.connect.accountApp.domain.settlement.adapter.out.persistence.jpa.SettlementJpaRepository;
 import com.connect.accountApp.domain.settlement.application.port.out.FindSettlementPort;
 import com.connect.accountApp.domain.settlement.application.port.out.SaveSettlementPort;
 import com.connect.accountApp.domain.settlement.application.port.out.command.ExpenseOfHouseholdCommand;
-import com.connect.accountApp.domain.settlement.adapter.out.persistence.jpa.model.SettlementJpaEntity;
-import com.connect.accountApp.domain.settlement.domain.model.Settlement;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.List;
@@ -16,27 +13,29 @@ import org.springframework.stereotype.Component;
 @Component
 public class SettlementPersistenceAdapter implements SaveSettlementPort, FindSettlementPort {
 
-  private final SettlementJpaRepository settlementJpaRepository;
+//  private final SettlementJpaRepository settlementJpaRepository;
   private final SettlementQueryRepository settlementQueryRepository;
-  private final SettlementMapper settlementMapper;
+//  private final SettlementMapper settlementMapper;
 
-  @Override
-  public Long saveSettlement(Settlement settlement) {
-    SettlementJpaEntity settlementJpaEntity = settlementMapper.mapToJpaEntity(settlement);
-    return settlementJpaRepository.save(settlementJpaEntity).getSettlementId();
-  }
+//  @Override
+//  public Long saveSettlement(Settlement settlement) {
+//    SettlementJpaEntity settlementJpaEntity = settlementMapper.mapToJpaEntity(settlement);
+//    return settlementJpaRepository.save(settlementJpaEntity).getSettlementId();
+//  }
 
   @Override
   public List<BigDecimal> findUserRealExpense(String userEmail, LocalDate startDate,
       LocalDate endDate) {
 
-    return settlementQueryRepository.findUserRealExpense(userEmail, startDate, endDate);
+//    return settlementQueryRepository.findUserRealExpense(userEmail, startDate, endDate);
+    return null;
   }
 
   @Override
   public List<ExpenseOfHouseholdCommand> findHouseholdExpenses(Long householdId,
       LocalDate startDate, LocalDate endDate) {
 
-    return settlementQueryRepository.findExpenseOfHousehold(householdId, startDate, endDate);
+//    return settlementQueryRepository.findExpenseOfHousehold(householdId, startDate, endDate);
+    return null;
   }
 }
